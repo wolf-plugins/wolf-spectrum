3f6903248c629600af10d05a8fa5e2df602a13bb
Patrick Desaulniers <desaulniers.patrick@carrefour.cegepvicto.ca>
2019-02-03 21:44:04 -0500

    Tweak provision script to use gcc-4.9

--- provision.sh
+++ provision.sh
@@ -6,12 +6,14 @@ export LC_ALL=en_US.UTF-8
 locale-gen en_US.UTF-8
 dpkg-reconfigure locales
 
+add-apt-repository ppa:ubuntu-toolchain-r/test
 apt-get update
-apt-get install -y g++ pkg-config git libx11-dev libgl1-mesa-dev liblo-dev libjack-dev libfreetype6-dev
+apt-get install -y g++ pkg-config git libx11-dev libgl1-mesa-dev liblo-dev libjack-dev gcc-4.9 g++-4.9
 
 #TODO: 
+#export CXX="g++-4.9" CC="gcc-4.9";
 #clone repo 
 #cd repo 
 #rm -r bin
-#ln -s /vagrant/bin bin/
+#ln -s /vagrant/bin bin
 #./make_release.sh (etc)
