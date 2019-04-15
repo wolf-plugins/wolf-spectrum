87d7aca59e295141a1e8019788267d3ef6d6cae7
Patrick Desaulniers <desaulniers.patrick@carrefour.cegepvicto.ca>
2019-04-14 20:53:44 -0400

    Add bundle source script

---
+++ bundle_source.sh
@@ -0,0 +1,15 @@
+PLUGIN_NAME=wolf-spectrum
+PLUGIN_VERSION=1.0.0
+TARBALL_NAME="$PLUGIN_NAME-v$PLUGIN_VERSION-source.tar.gz"
+
+rm -rf "$PLUGIN_NAME"
+git clone --recursive "https://github.com/pdesaulniers/$PLUGIN_NAME.git" --branch "v$PLUGIN_VERSION" --single-branch
+cd "$PLUGIN_NAME"
+find . -name ".git" -type f -delete
+rm -rf .git
+find . -name ".gitmodules" -type f -delete
+cd ..
+tar -zcvf "$TARBALL_NAME" "$PLUGIN_NAME/"
+rm -rf "$PLUGIN_NAME/"
+
+gpg --armor --detach-sign "$TARBALL_NAME"
